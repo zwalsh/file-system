f220839ca103099064673cc80b2ce5e7527ccef3
Lindsay <richter.li@husky.neu.edu>
2017-11-28 12:52:27 -0500

    changes on structure implementation

--- storage.c
+++ storage.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "storage.h"
 #include "pages.h"
@@ -14,32 +15,30 @@ const int INODE_PAGE = 2;
 const int DATA_BLOCK_PAGE = 20;
 
 typedef struct file_entry {
-	// file name
 	char* name;
-	// iNode number
 	int iNode_number;
-	// 0 if file isn't a dir
-	int number_of_entries;
-	// null if file isn't a dir
-	struct file_entry* entries;
 } file_entry;
+
+typedef struct directory {
+	int number_of_entries;
+	file_entry* entries;
+} directory;
 
 // contains metadata for each file or directory
 typedef struct iNode {
 	// indicates object type (e.g. dir, file) and permissions
 	int mode;
-	// carries additional metadata 
-	// if file is a directory, will store number of entries and
-	file_entry* dir;
-	// size of file this iNode represents
+	int num_hard_links;
+	int user_id;
+	int group_id;
+	// size of file this iNode represents in bytes
 	int size;
-	// user that created the file
-	// not sure if we need this? It's in my notes but seems mostly useless
-	int user;
-	// time this file was created
-	int time_created;
+	time_t last_time_accessed;
+	time_t last_time_modified;
+	time_t last_time_status_change;
 	// array of blocks that store this thing
 	int data_block_ids[10];
+	int indirect_data_block_id;
 } iNode;
 
 void* 
